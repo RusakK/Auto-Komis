e2b288869705dd30a4f0976c8c7954d167793b72
krissuNew <krzysztofrusak91@gmail.com>
2018-07-14 15:12:29 +0200

    lista samochodów się wyświetla, szczegóły samochodu również

---
+++ src/main/java/sda/project/autoKomis/repository/CarRepository.java
@@ -0,0 +1,13 @@
+package sda.project.autoKomis.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import sda.project.autoKomis.model.Car;
+
+import java.util.List;
+
+public interface CarRepository extends CrudRepository<Car, Integer> {
+
+    @Query("SELECT c FROM Car c WHERE c.sold = false")
+    List<Car> findNotSoldCar();
+}
